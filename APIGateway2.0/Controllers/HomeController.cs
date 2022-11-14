be0ce10d873d31698f5aaf1b16435b2c270490ce
rechenwerk-a <adrian.vinojcic@gmail.com>
2022-11-14 21:04:54 +0100

    Create Homepage

--- APIGateway2.0/Controllers/HomeController.cs
+++ APIGateway2.0/Controllers/HomeController.cs
@@ -1,26 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace APIGateway2._0.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        public ActionResult Index() =>
+            View();
 
-        public ActionResult BankAgents()
-        {
-            return View();
-        }
+        public ActionResult BankAgents() =>
+            View();
 
-        public ActionResult ShopAgents()
-        {
-            return View();
-        }
+        public ActionResult ShopAgents() =>
+            View();
     }
 }
