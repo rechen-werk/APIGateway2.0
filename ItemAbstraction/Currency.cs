e29d1a944ea18cda8844e1b07e15a2e4e208779e
rechenwerk-a <adrian.vinojcic@gmail.com>
2022-11-15 16:33:44 +0100

    Add Currency and ItemList

---
+++ ItemAbstraction/Currency.cs
@@ -0,0 +1,7 @@
+namespace ItemAbstraction
+{
+    public enum Currency
+    {
+        EUR, USD, JPY, GBP, AUD, CAD, CHF
+    }
+}
--- ItemAbstraction/Items.cs
+++ ItemAbstraction/Items.cs
@@ -1,7 +1,81 @@
 namespace ItemAbstraction
 {
-    public class Items
+    public sealed class Item
     {
-        
+        public int Price { get; }
+        public Currency Currency { get; }
+        public ItemType Type { get; }
+
+        public Item(ItemType type, int price, Currency currency)
+        {
+            Type = type;
+            Price = price;
+            Currency = currency;
+        }
     }
+
+    public enum ItemType
+    {
+        Apple,
+        Banana,
+        Pear,
+        Grapes,
+        Avocado,
+        Pineapple,
+        Grapefruit,
+        Watermelon,
+        Orange,
+        Coconut,
+        Kiwi,
+        Mango,
+        Corn,
+        Mushroom,
+        Broccoli,
+        Cucumber,
+        Pepper,
+        Carrot,
+        Tomato,
+        Cabbage,
+        Potato,
+        Lettuce,
+        Onion,
+        Radish,
+        Spinach,
+        Peas,
+        Beans,
+        Chili,
+        Milk,
+        Bread,
+        Water,
+        Yoghurt,
+        Cheese,
+        Butter,
+        Eggs,
+        Bacon,
+        Pork,
+        Beef,
+        Sausages,
+        Coffee,
+        Pasta,
+        Oil,
+        Jam,
+        Mustard,
+        Salt,
+        Mayonnaise,
+        Ketchup,
+        Ginger,
+        Oregano,
+        Basil,
+        Sugar,
+        Garlic,
+        Flour,
+        Vinegar,
+        Vanilla,
+        Rice,
+        Fish,
+        Chocolate,
+        Chips,
+        Cookies
+    }
+
 }
