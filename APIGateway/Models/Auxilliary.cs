5556039c3912226c58f5065fbb13251fbc93a4a5
rechenwerk-a <adrian.vinojcic@gmail.com>
2022-11-26 19:51:23 +0100

    Cleanup Auxiliary

--- APIGateway/Models/Auxilliary.cs
+++ APIGateway/Models/Auxilliary.cs
@@ -4,114 +4,60 @@ namespace APIGateway.Models
 {
     public static class Auxilliary
     {
-        public static TimeSpan Millisecond(this double milliseconds)
-        {
-            return TimeSpan.FromMilliseconds(milliseconds);
-        }
+        public static TimeSpan Millisecond(this double milliseconds) =>
+            TimeSpan.FromMilliseconds(milliseconds);
+        public static TimeSpan Milliseconds(this double milliseconds) =>
+            TimeSpan.FromMilliseconds(milliseconds);
+        public static TimeSpan Millisecond(this int milliseconds) =>
+            TimeSpan.FromMilliseconds(milliseconds);
+        public static TimeSpan Milliseconds(this int milliseconds) =>
+            TimeSpan.FromMilliseconds(milliseconds);
 
-        public static TimeSpan Second(this double seconds)
-        {
-            return TimeSpan.FromSeconds(seconds);
-        }
+        public static TimeSpan Second(this double seconds) =>
+            TimeSpan.FromSeconds(seconds);
+        public static TimeSpan Seconds(this double seconds) =>
+            TimeSpan.FromSeconds(seconds);
+        public static TimeSpan Second(this int seconds) =>
+            TimeSpan.FromSeconds(seconds);
+        public static TimeSpan Seconds(this int seconds) =>
+            TimeSpan.FromSeconds(seconds);
 
-        public static TimeSpan Minute(this double minutes)
-        {
-            return TimeSpan.FromMinutes(minutes);
-        }
+        public static TimeSpan Minute(this double minutes) =>
+            TimeSpan.FromMinutes(minutes);
+        public static TimeSpan Minutes(this double minutes) =>
+            TimeSpan.FromMinutes(minutes);
+        public static TimeSpan Minute(this int minutes) =>
+            TimeSpan.FromMinutes(minutes);
+        public static TimeSpan Minutes(this int minutes) =>
+            TimeSpan.FromMinutes(minutes);
 
-        public static TimeSpan Hour(this double hours)
-        {
-            return TimeSpan.FromHours(hours);
-        }
+        public static TimeSpan Hour(this double hours) =>
+            TimeSpan.FromHours(hours);
+        public static TimeSpan Hours(this double hours) =>
+            TimeSpan.FromHours(hours);
+        public static TimeSpan Hour(this int hours) =>
+            TimeSpan.FromHours(hours);
+        public static TimeSpan Hours(this int hours) =>
+            TimeSpan.FromHours(hours);
 
-        public static TimeSpan Day(this double days)
-        {
-            return TimeSpan.FromDays(days);
-        }
 
-        public static TimeSpan Milliseconds(this double milliseconds)
-        {
-            return TimeSpan.FromMilliseconds(milliseconds);
-        }
-
-        public static TimeSpan Seconds(this double seconds)
-        {
-            return TimeSpan.FromSeconds(seconds);
-        }
-
-        public static TimeSpan Minutes(this double minutes)
-        {
-            return TimeSpan.FromMinutes(minutes);
-        }
-
-        public static TimeSpan Hours(this double hours)
-        {
-            return TimeSpan.FromHours(hours);
-        }
-
-        public static TimeSpan Days(this double days)
-        {
-            return TimeSpan.FromDays(days);
-        }
-
-        public static TimeSpan Millisecond(this int milliseconds)
-        {
-            return TimeSpan.FromMilliseconds(milliseconds);
-        }
-
-        public static TimeSpan Second(this int seconds)
-        {
-            return TimeSpan.FromSeconds(seconds);
-        }
-
-        public static TimeSpan Minute(this int minutes)
-        {
-            return TimeSpan.FromMinutes(minutes);
-        }
-
-        public static TimeSpan Hour(this int hours)
-        {
-            return TimeSpan.FromHours(hours);
-        }
-
-        public static TimeSpan Day(this int days)
-        {
-            return TimeSpan.FromDays(days);
-        }
-
-        public static TimeSpan Milliseconds(this int milliseconds)
-        {
-            return TimeSpan.FromMilliseconds(milliseconds);
-        }
-
-        public static TimeSpan Seconds(this int seconds)
-        {
-            return TimeSpan.FromSeconds(seconds);
-        }
-
-        public static TimeSpan Minutes(this int minutes)
-        {
-            return TimeSpan.FromMinutes(minutes);
-        }
-
-        public static TimeSpan Hours(this int hours)
-        {
-            return TimeSpan.FromHours(hours);
-        }
-
-        public static TimeSpan Days(this int days)
-        {
-            return TimeSpan.FromDays(days);
-        }
+        public static TimeSpan Day(this double days) =>
+            TimeSpan.FromDays(days);
+        public static TimeSpan Days(this double days) =>
+            TimeSpan.FromDays(days);
+        public static TimeSpan Day(this int days) =>
+            TimeSpan.FromDays(days);
+        public static TimeSpan Days(this int days) =>
+            TimeSpan.FromDays(days);
     }
 
     public class Half
     {
-        public static double A = 0.5d;
+        public const double A = 0.5d;
     }
 
     public class Quarter
     {
-        public static double A = 0.25d;
+        public const double A = 0.25d;
     }
 }
